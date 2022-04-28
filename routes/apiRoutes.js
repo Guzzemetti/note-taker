5b3e34cdde04075365fbc744a827f4dd2f7d0eb4
John Guzzetta <johnguzzettaiv@gmail.com>
2022-04-28 17:47:33 -0400

    Updated file structure, added several files with base code

---
+++ routes/apiRoutes.js
@@ -0,0 +1,11 @@
+// require router and db items needed
+const { route } = require('express/lib/application');
+const path = require('path');
+const router = require('express').Router();
+// const db = require('../db/db.json');
+
+
+// set up get/post/delete methods as responses to the database
+
+// export to the router
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,22 @@
+// required routes for our api/html
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const notes = require('./routes/notes')
+
+// initializes app and creates a port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// TODO: set up body parsing, static, and route the middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.use(express.static('public'));
+
+
+// starts the server on the port
+app.listen(PORT, () => console.log('This port is listening at ${PORT}'));
